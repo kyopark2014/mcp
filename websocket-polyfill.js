e5160d61603711461b66992bfda0f764a783da1e
Kyoungsu Park <52392004+kyopark2014@users.noreply.github.com>
2025-10-04 10:47:42 +0900

    Delete websocket-polyfill.js

--- websocket-polyfill.js
+++
@@ -1,7 +0,0 @@
-// WebSocket polyfill for Node.js environment
-if (typeof global !== 'undefined' && !global.WebSocket) {
-  const WebSocket = require('ws');
-  global.WebSocket = WebSocket;
-  global.window = global.window || {};
-  global.window.WebSocket = WebSocket;
-}
